c9c4bcf3f600ac543e2758fa8ac2292cee74aee2
edn <45616806+theednorc@users.noreply.github.com>
2022-03-10 03:56:12 +0630

    upd

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_search_flutter/models/movie.dart';
+import 'package:movieapp_search_flutter/network/api.dart';
+import 'package:movieapp_search_flutter/components/movie_list.dart';
+import 'package:movieapp_search_flutter/pages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Movie>? popularMovies;
+  List<Movie>? nowPlayingMovies;
+
+  loadPopular() {
+    API().getPopular().then((value) {
+      setState(() {
+        popularMovies = value;
+      });
+    });
+
+    API().getNowPlaying().then((value) {
+      setState(() {
+        nowPlayingMovies = value;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadPopular();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Movie App"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SearchPage()));
+                },
+                icon: const Icon(Icons.search))
+          ],
+        ),
+        body: Column(children: [
+          nowPlayingMovies == null
+              ? const Center(child: CircularProgressIndicator())
+              : MovieList(
+                  list: nowPlayingMovies!,
+                  title: "Now Playing",
+                ),
+          popularMovies == null
+              ? const Center(child: CircularProgressIndicator())
+              : MovieList(
+                  list: popularMovies!,
+                  title: "Popular",
+                )
+        ]));
+  }
+}
